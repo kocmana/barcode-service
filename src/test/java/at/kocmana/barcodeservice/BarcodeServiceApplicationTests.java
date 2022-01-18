c5a8548ea0cf2f21c04e46b0621cd97ed45e7a49
kocmana <andreas.kocman@gmail.com>
2022-01-18 23:11:06 +0100

    #1 Initial project stub added

---
+++ src/test/java/at/kocmana/barcodeservice/BarcodeServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package at.kocmana.barcodeservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BarcodeServiceApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
