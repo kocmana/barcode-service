82e30675e0558f1d4cc6ddd0fc3d8ddea0c01eee
kocmana <andreas.kocman@gmail.com>
2022-02-22 22:54:55 +0100

    Disable service tests (see issue #5)

--- src/test/java/at/kocmana/barcodeservice/upc/UpcServiceTest.java
+++ src/test/java/at/kocmana/barcodeservice/upc/UpcServiceTest.java
@@ -6,6 +6,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 import at.kocmana.barcodeservice.upc.model.UpcA;
 import at.kocmana.barcodeservice.upc.model.UpcE;
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -17,6 +18,7 @@ class UpcServiceTest {
   UpcService underTest;
 
   @Test
+  @Disabled("Needs fixing as part of issue #5")
   void whenGenerateUpcA_CorrectImageIsReturned() {
     //given
     var upcA = new UpcA("12345678901");
@@ -31,6 +33,7 @@ class UpcServiceTest {
   }
 
   @Test
+  @Disabled("Needs fixing as part of issue #5")
   void whenGenerateUpcE_CorrectImageIsReturned() {
     //given
     var upcE = new UpcE("1234567");
